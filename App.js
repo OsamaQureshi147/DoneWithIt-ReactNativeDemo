79599328121758ac8b7dadf19773617c74d2a120
OsamaQureshi147 <osamahsan@gmail.com>
2021-08-08 18:28:39 +0500

    code cleaning

--- App.js
+++ App.js
@@ -1,46 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Switch } from 'react-native';
-import AppPicker from './app/components/AppPicker';
-import AppTextInput from './app/components/AppTextInput';
+
+import LoginScreen from './app/screens/LoginScreen';
 
 
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import Screen from './app/components/Screen';
-import AccountScreen from './app/screens/AccountScreen';
-import HomeScreen from './app/screens/HomeScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Electronics", value: 3 },
-]
 
 export default function App() {
-  const [isNew, setIsNew] = useState(false)
-  const [category, setCategory] = useState(categories[0]);
 
   return (
-    <Screen>
-      {/* <ListItem title="My Title" ImageComponent={<Icon name="email" />} /> */}
-      {/* <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} /> */}
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={item => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-
-
+    <>
+      <LoginScreen />
+    </>
   );
 }
 
