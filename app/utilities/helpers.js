3760f2b83f5852a986aef62384840a250a394fbf
OsamaQureshi147 <osamahsan@gmail.com>
2021-08-08 18:28:02 +0500

    defines login validation schema

---
+++ app/utilities/helpers.js
@@ -0,0 +1,6 @@
+import * as Yup from 'yup';
+
+export const loginValidationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label('Email'),
+    password: Yup.string().required().min(4).label('Password')
+})
