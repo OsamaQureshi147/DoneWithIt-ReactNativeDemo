798a4727f615e96efdd09ab73958350cd28a611c
Osama <osamahsan@gmail.com>
2021-09-06 20:07:29 +0500

    adds register screen validation schema

--- app/utilities/helpers.js
+++ app/utilities/helpers.js
@@ -4,3 +4,9 @@ export const loginValidationSchema = Yup.object().shape({
     email: Yup.string().required().email().label('Email'),
     password: Yup.string().required().min(4).label('Password')
 })
+
+export const registerValidationSchema = Yup.object().shape({
+    name: Yup.string().required().label('Name'),
+    email: Yup.string().required().email().label('Email'),
+    password: Yup.string().required().min(4).label('Password')
+})
